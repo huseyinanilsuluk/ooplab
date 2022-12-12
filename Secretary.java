5a62f19f498566c1b09c1ed4167cf4a092ed8fd3
huseyinanilsuluk <119812419+huseyinanilsuluk@users.noreply.github.com>
2022-12-12 17:39:42 +0300

    Update Secretary.java

--- Secretary.java
+++ Secretary.java
@@ -10,39 +10,36 @@ package ooplab;
  */
 import static java.util.Collections.list;
 import java.util.List;
-public class Secretary extends Staff
-{
-    public Secretary(String name,int id, String taskInformation, String workingHours)
-    {
-        super(name,id,taskInformation,workingHours);
-//        this.id=id;
-//        this.taskInformation=taskInformation;
-//        this.workingHours=workingHours;
-    }
-    
-    //hoca atama
-    public Courses appointLecturer(List course, List lecturer)
-    {
-        if(lecturer.getNumCourses()<10)
-        {
-            lecturer.addCourses(course);
-            course.setLecturer(lecturer);
-        }
-        else
-        {
-            System.out.println("This lecturer access to max course number");
-        }
-        return null;
-    }
-    
-    //ders hocasını değiştirme 
-    public Courses changeLecturer(List course, List lecturer)
-    {
-        course.getLecturer().deleteCourses(course);
-        course.setLecturer(lecturer);
-        lecturer.deleteCourses(course);
-        return (Courses) course;
-    }
-    
-    
+public class Secretary extends Staff {
+	
+	 
+
+	public Secretary(int id, String taskInformation, String workingHours) {
+		super(id, taskInformation, workingHours);
+		
+	}
+	
+	public Courses hocaAtama(Courses course, Lecturer lecturer) {
+		if(lecturer.getNumCourses()<10) {
+		
+		lecturer.addCourse(course);
+		course.setLecturer(lecturer); 
+		
+		}
+		else {
+			System.out.println("This lecturer Access to max course number.");
+		}
+		return course;
+		
+	}
+	
+	public Courses hocaDegistirme(Courses course, Lecturer lecturer) {
+	
+		course.getLecturer().deleteCourses(course);
+		course.setLecturer(lecturer); 
+		lecturer.addCourse(course);
+		return course;
+		
+	}	
+	
 }
