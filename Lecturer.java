93f093338922f59705f87e76edd2174e147899db
huseyinanilsuluk <119812419+huseyinanilsuluk@users.noreply.github.com>
2022-12-12 17:35:27 +0300

    Update Lecturer.java

--- Lecturer.java
+++ Lecturer.java
@@ -11,92 +11,84 @@ import java.util.List;
  * @author sueda
  */
 
-public class Lecturer
-{
-    private int id;
-    private String name;
-    private List courses;
-    private int numCourses;
-    private static final int MAX_COURSES =10;
+public class Lecturer {
+	
+	
+	private int id;
+	private String name;     
+    ArrayList<Courses>courses = new ArrayList<Courses>();
+	private static final int MAX_COURSES =10;
+	
+	private int numCourses;
+
+	public Lecturer(int id, String name) {
+		
+		this.id = id;
+		this.name = name;
+		//courses = new Arraylist<Courses>();
+		courses =new ArrayList<Courses>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList getCourses() {
+		return courses;
+	}
+
+	public void setCourses(ArrayList courses) {
+		this.courses = courses;
+	}
+
+	public int getNumCourses() {
+		return numCourses;
+	}
+
+	public void setNumCourses(int numCourses) {
+		this.numCourses = numCourses;
+	}
+
+	public void addCourse(Courses course) {
+	
+		  if(numCourses<MAX_COURSES)
+	        {
+	            courses.add(numCourses, (Courses)course); 
+	            numCourses++;
+	        }
+	       /* else if(numCourses==MAX_COURSES)
+	        {
+	             System.out.println("This lecturer access to max course number.!");   
+	        }*/
+	}
    
-    
-    public Lecturer(int id, String name)
-    {
-      this.id=id;
-      this.name=name;
-      //courses=new Courses[MAX_COURSES];
-    }
-    
-    public void setId(int id)
-    {
-        this.id=id;
-    }
-    public int getId()
-    {
-        return id;
-    }
-    public void setName(String name)
-    {
-        this.name=name;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public void setCourses(List course)
-    {
-        this.courses=courses;
-    }
-    public List getCourses()
-    {
-        return courses;
-    }
-    public void setNumCourses(int numCourses)
-    {
-        this.numCourses=numCourses;
-    }
-    public int getNumCourses()
-    {
-        return numCourses;
-    }
-    
-    
-    //kurs ekleme
-    public void addCourses(List course)
-    { 
-       
-        if(numCourses<MAX_COURSES)
-        {
-            courses.set(numCourses, (Courses)course); 
-            numCourses++;
-        }
-        else if(numCourses==MAX_COURSES)
-        {
-             System.out.println("This lecturer access to max course number.!");   
-        }
-    }
-    
-    //kurs silme 
-    public void deleteCourses(List course)
-    {
-        if(numCourses>0){
-            for(int i=0;i<numCourses;i++)
-            {
-                if(courses.get(i)==course)
-                {
-                    //courses.romeove(i);
-                    numCourses--;
-                }
-            }
-        }
-    }
-    
-    //kursları gösterme 
-    public void showCourses()
-    {
-        for(int i=0;i<numCourses;i++)
-        {
-            System.out.println(courses.get(i).getName());
-        }
-    }
+    	 public void deleteCourses(Courses course)
+    	    {
+    	        if(numCourses>0){
+    	           
+    	                    courses.remove(course);
+    	                    numCourses--;
+    	                
+    	            
+    	        }
+    	    }
+		
+       public void showCourses() {
+		for(int i =0; i<courses.size(); i++) {
+			System.out.println(""+ name + " " + courses.get(i).getName());
+		}
+		
+	}
 }
